de7bea79068760bfdcb6b228b52f121f22f4f19b
StarLikeRain <pengyaokang@qq.com>
2016-12-25 16:55:56 +0800

    add nav decoration

--- js/page-main.js
+++ js/page-main.js
@@ -1,3 +1,14 @@
 /**
  * Created by StarLikeRain on 25/12/2016.
  */
+let $nav_child_list = $('.nav-child-list');
+let $nav_guide = $('#nav-guide');
+$nav_child_list.on('mouseenter', () => {
+    $nav_child_list.closest('li').addClass('active');
+}).on('mouseleave', () => {
+    $nav_guide.children('li').removeClass('active');
+})
+
+// $nav_guide.on('mouseenter', function (e) {
+//
+// })
