13f094b23e6c6290cb75b913dbd0d1695d79ba07
StarLikeRain <pengyaokang@qq.com>
2016-12-31 10:56:45 +0800

    把css3-hover.js改为es6语法

--- js/css3-hover.js
+++ js/css3-hover.js
@@ -1,36 +1,39 @@
 /**
  * Created by StarLikeRain on 25/12/2016.
  */
-$('#test').on('mousemove', function (e) {
+let Ele_text = $('#test');
+let Ele_banner = $('#banner');
 
-    var offset = $('#test').offset()
+Ele_text.on('mousemove', function (e) {
+
+    let offset = Ele_text.offset();
 
     // 鼠标偏移
-    var x = e.pageX - offset.left
-    var y = e.pageY - offset.top
+    let x = e.pageX - offset.left;
+    let y = e.pageY - offset.top;
 
     // 中间点位
-    var centerX = $('#test').outerWidth() / 2 //877.5
-    var centerY = $('#test').outerHeight() / 2 //250
+    let centerX = Ele_text.outerWidth() / 2; //877.5
+    let centerY = Ele_text.outerHeight() / 2; //250
 
     // 距中鼠标偏移 " + - "
-    var deltaX = x - centerX
-    var deltaY = y - centerY
+    let deltaX = x - centerX;
+    let deltaY = y - centerY;
 
-    var percentX = deltaX / centerX
-    var percentY = deltaY / centerY
+    let percentX = deltaX / centerX;
+    let percentY = deltaY / centerY;
 
-    var deg = 10
+    let deg = 10;
 
-    $('#banner').css({
+    Ele_banner.css({
         transform: 'rotateX(' + deg * -percentY + 'deg)' +
         ' rotateY(' + deg * percentX + 'deg)' +
         'translate3d(0,0,50px  )'
-    })
-})
+    })m
+});
 
-$('#test').on('mouseleave', function () {
-    $('#banner').css({
+Ele_text.on('mouseleave', function () {
+    Ele_banner.css({
         transform: ''
     })
-})
+});
